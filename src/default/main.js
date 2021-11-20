56db06a460c93e2244a9c3a6502ccec1e3dd4d06
甘霖 <thelmg@163.com>
2021-11-20 15:09:54 +0800

    增加自动格式化

--- src/default/main.js
+++ src/default/main.js
@@ -1,11 +1,9 @@
 import "./main.less";
 
+document.querySelector("h2").innerText = `${import.meta.env.VITE_APP_NAME}`;
 
-
-document.querySelector("h2").innerText = `${import.meta.env.VITE_APP_NAME}`
-
-document.querySelector("main p").innerHTML = `${
-    Object.values(app_pages).map((pages, index) => {
-        return `<a href="${app_base}${pages}">${Object.keys(app_pages)[index]}</a>`
-    }).join("")
-}`
+document.querySelector("main p").innerHTML = `${Object.values(app_pages)
+  .map((pages, index) => {
+    return `<a href="${app_base}${pages}">${Object.keys(app_pages)[index]}</a>`;
+  })
+  .join("")}`;
--- utile.js
+++ utile.js
@@ -2,17 +2,17 @@ const path = require('path');
 var glob = require("glob");
 
 function entrys() {
-    let entries = {};
-    const paths = glob.sync('src/*/*.html')
-    paths.map(file => {
-        entries[path.basename(path.dirname(file))] = "/"+file;
-    });
-    return entries
+  let entries = {};
+  const paths = glob.sync('src/*/*.html')
+  paths.map(file => {
+    entries[path.basename(path.dirname(file))] = "/" + file;
+  });
+  return entries
 }
 
 export default {
-    entrys: entrys(),
-    base: "/"
+  entrys: entrys(),
+  base: "/"
 }
 
 
